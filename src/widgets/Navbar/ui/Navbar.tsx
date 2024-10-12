e9737e83b0bd36fd95ebf6e5e77dc5af52cf4265
Dmitriy <dimentroe.web@gmail.com>
2024-10-12 21:36:52 +0300

    Создание первых компонентов

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,19 @@
+import { AppLink } from "shared/ui/AppLink/AppLink";
+import { classNames } from "shared/lib/classNames/classNames";
+
+import styles from "./Navbar.module.scss";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(styles.navbar, {}, [className])}>
+      <div className={classNames(styles.links)}>
+        <AppLink to="/">Главная</AppLink>
+        <AppLink to="/about">О сайте</AppLink>
+      </div>
+    </div>
+  );
+};
